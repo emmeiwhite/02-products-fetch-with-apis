32add95f0fbfb7f5f6789c71035f1c31b55f54b3
emmeiwhite <emmeiwhite@gmail.com>
2024-10-11 12:19:13 +0530

    WIP - rendering array with map() within template strings

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const url = 'https://www.course-api.com/javascript-store-productsss'
+const url = 'https://www.course-api.com/javascript-store-products'
 
 const resultDIV = document.querySelector('.products-center')
 
@@ -6,18 +6,41 @@ const fetchProducts = async () => {
   // AJAX section where data is to be loaded from an external API will have 3 states and only one state will be rendered within this piece of UI at a time based on the fetched data's outcome
 
   /** 1. Loading ... */
-  resultDIV.textContent = 'Loading ...'
+  resultDIV.innerHTML = `<div class="loading"></div>`
 
   const response = await fetch(url)
   try {
     if (!response.ok) {
-      throw new Error("Error: Couldn't fetch the data!")
+      throw new Error("Couldn't fetch the data!")
     }
     const data = await response.json()
     console.log(data)
+    const productsUI = data.map(product => {
+      const {} = product
+      return `<div class="products-container">
+          <a
+            href="product.html"
+            class="single-product"
+          >
+            <img
+              src="./couch.jpg"
+              alt="sofa"
+              class="img single-product-img"
+            />
+
+            <footer>
+              <h5 class="name">product title</h5>
+              <span class="price">$9.99</span>
+            </footer>
+          </a>
+        </div>
+`
+    })
+
+    resultDIV.textContent = productsUI
   } catch (error) {
     console.log(error)
-    resultDIV.textContent = error
+    resultDIV.innerHTML = `<p class="error">${error}</p>`
   }
 }
 
